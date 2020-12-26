4421077f31425c7c770e2f789828fd0d1e3e7bee
Alejandro Carrillo <alejandroecarrilloa@gmail.com>
2020-12-26 12:03:58 -0600

    2nd week react

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap'; 
+
+
+class CampsiteInfo extends Component{
+    constructor(props){
+        super(props);
+    }
+
+
+
+    renderSelectedCampsite(campsite){
+        return (
+            <div className='col-md-5 m-1'>
+                 <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name}/>
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+
+    renderComments(commentsArray){
+        if (commentsArray){
+            return(
+                <div className='col-md-5 m-1' >
+                    <h4>Comments</h4>
+                    {
+                    commentsArray.map( x => {
+                        return (
+                            <div key={x.id}>
+                            <p>{x.text} <br></br> -- {x.author},  {new Intl.DateTimeFormat('en-Us',{ year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(x.date)))}</p>
+                            </div>)
+                    })
+                    }
+                </div>
+            )
+        }
+        <div></div>
+    }
+
+    render(){
+        if(this.props.campsite){
+            return(
+                <div>
+                    <div className='row' >
+                        {this.renderSelectedCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                </div>
+            )
+        }
+        else{
+            return(
+                <div></div>
+            )
+        }
+    }
+
+
+
+}
+
+export default CampsiteInfo;
