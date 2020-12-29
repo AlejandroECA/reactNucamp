7a02abe51c958497e9c751ff7a438a129a49b593
Alejandro Carrillo <alejandroecarrilloa@gmail.com>
2020-12-29 13:09:03 -0600

    footer & header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent'
 import './App.css';
+import {BrowserRouter} from 'react-router-dom'
 import { CAMPSITES } from './shared/campsites'
 
 class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <Main />
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                    <Main />
+                </div>
+            </BrowserRouter>
         );
     }
-    
+
 }
 
 export default App;
--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,67 +1,59 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap'; 
 
 
-class CampsiteInfo extends Component{
-    constructor(props){
-        super(props);
-    }
 
+function RenderSelectedCampsite({campsite}){
+    return (
+        <div className='col-md-5 m-1'>
+                <Card>
+                <CardImg top src={campsite.image} alt={campsite.name}/>
+                <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    )
+}
 
-
-    renderSelectedCampsite(campsite){
-        return (
-            <div className='col-md-5 m-1'>
-                 <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name}/>
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
+function RenderComments({comments}){
+    if (comments){
+        return(
+            <div className='col-md-5 m-1' >
+                <h4>Comments</h4>
+                {
+                comments.map( x => {
+                    return (
+                        <div key={x.id}>
+                        <p>{x.text} <br></br> -- {x.author},  {new Intl.DateTimeFormat('en-Us',{ year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(x.date)))}</p>
+                        </div>)
+                })
+                }
             </div>
         )
     }
-
-    renderComments(commentsArray){
-        if (commentsArray){
-            return(
-                <div className='col-md-5 m-1' >
-                    <h4>Comments</h4>
-                    {
-                    commentsArray.map( x => {
-                        return (
-                            <div key={x.id}>
-                            <p>{x.text} <br></br> -- {x.author},  {new Intl.DateTimeFormat('en-Us',{ year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(x.date)))}</p>
-                            </div>)
-                    })
-                    }
-                </div>
-            )
-        }
-        <div></div>
-    }
-
-    render(){
-        if(this.props.campsite){
-            return(
-                <div className='container'>
-                    <div className='row' >
-                        {this.renderSelectedCampsite(this.props.campsite)}
-                        {this.renderComments(this.props.campsite.comments)}
-                    </div>
-                </div>
-            )
-        }
-        else{
-            return(
-                <div></div>
-            )
-        }
-    }
-
-
-
+    <div></div>
 }
+
+function CampsiteInfo(props){
+if(props.campsite){
+    return(
+        <div className='container'>
+            <div className='row' >
+                <RenderSelectedCampsite campsite={props.campsite} />
+                <RenderComments comments={props.campsite.comments} />
+            </div>
+        </div>
+    )
+}
+else{
+    return(
+        <div></div>
+    )
+}
+}   
+    
+
 
 export default CampsiteInfo;
